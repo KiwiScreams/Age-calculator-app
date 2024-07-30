ee197642e8b56345bbdd2a6abe624e9ff85ada27
KiwiScreams <screamskiwi3@gmail.com>
2024-07-30 21:33:49 +0400

    js

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const dayInput = document.getElementById('day');
+const monthInput = document.getElementById('month');
+const yearInput = document.getElementById('year');
+const calculateButton = document.getElementById('btn');
+
+const dd = document.getElementById('dd');
+const mm = document.getElementById('mm');
+const yy = document.getElementById('yy');
